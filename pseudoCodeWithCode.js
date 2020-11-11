cd91f7fb208a07033bcf8da4f9ad796dd931630f
JonLyn <jonlyn22@gmail.com>
2020-11-10 23:52:19 -0500

    correct move and total move counter completed :jigsaw:

--- pseudoCodeWithCode.js
+++ pseudoCodeWithCode.js
@@ -1,88 +1,83 @@
-$(function () {
+
 
 // Impuzzible
 
 // A landing page introducing the game and rules
-
-
 // Point users to click link to go to game page
 
+// Create puzzle pieces
+for (let i = 0; i < 16; i++) {
+    $('.piecesContainer').append(`<div class='piece jigsaw${i}'></div>`);
+}
+
+// Create piece slots
+for (let i = 0; i < 16; i++) {
+    $('.puzzleContainer').append(`<div class='pieceSlot slot${i}'></div>`);
+}
 
 // Shuffle puzzle pieces in the piece container using a randomizer
-    // Get random x and y positioning 
-    // Apply random positioning to each piece 
+// Get random x and y positioning 
+// Apply random positioning to each piece 
 
 
 // Add draggable state to puzzle pieces so they can be dragged and snap to puzzle slots
-    $('.piece').draggable({
-        snap: ".pieceSlot",
-        containment: '.wrapper',
-        revert: function(dropOkay) {
-            if (dropOkay) {
-                console.log('good');
-            } else {
-                count += 1;
-                console.log(count);
-
-                if (count === 3) {
-                    console.log('dead');
-                }
-            }
-        }
-    });
+$('.piece').draggable({
+    snap: '.pieceSlot',
+    containment: '.wrapper',
+    snapTolerance: 50,
+    zIndex: 100
+});
 
 // Apply a unique ID/index to each puzzle piece
-    
-
-    const piece = document.getElementsByClassName('piece');
-    let piecesArray = Array.from(piece);
-    // console.log(piecesArray);
+const piece = document.getElementsByClassName('piece');
+const piecesArray = Array.from(piece);
 
 // Apply a unique ID/index to each puzzle slot
-    const slot = document.getElementsByClassName('pieceSlot')
-    const slotArray = Array.from(slot);
-    console.log(slotArray);
+const slot = document.getElementsByClassName('pieceSlot')
+const slotArray = Array.from(slot);
+
+// Add droppable state to puzlze slots so they can accept correct puzzle piece
+let correctCount = 0;
+for (let i = 0; i <= 16; i++) {
+    $(slotArray[i]).droppable({
+        accept: $(piecesArray[i]),
+        drop: $(slotArray[i]).on('drop', function (event, ui) {
+            console.log('works');
+            $(piecesArray[i]).draggable('disable');
+            correctCount += 1;
+            if (correctCount === 16) {
+                alert('you win!');
+            }
+            console.log(correctCount);
+        })
+    })
+}
+
+// Count moves.  If moves = 20, game over
+let moveCount = 0;
+$('.puzzleContainer').droppable({
+    accept: $('.piece'),
+    drop: function (event, ui) {
+        moveCount += 1;
+        if (moveCount === 21) {
+            alert('you lose!');
+        }
+        console.log(moveCount);
+    }
+})
+
+// MAYBE USE MOVE COUNT - CORRECT COUNT TO COUNT LIVES
+
+
+
+
+
+
+
+
 
 // Check if puzzle piece ID matches puzzle slot ID
-    let count = 0;
 
-    for (let i = 0; i <= 16; i++) {
-        // console.log($(piecesArray[i]));
-        $(slotArray[i]).droppable({
-            accept: $(piecesArray[i]),
-            drop: function (event, ui) {
-                // $(this).find('p').html('dropped');
-                // console.log('yes');
-                console.log('works');
-                
-            
-                }
-            })
-
-        // $('.s1').droppable({
-        //     // accept: '.a1',
-        //     accept: function () {
-        //         if ($('.a1').hasClass(`a1`) == true) {
-        //             console.log('yessss');
-        //             return '.a1';
-        //         }  
-        //     },    
-            // function () {
-            //     if (true) {
-            //         console.log('yay');
-            //     }
-            // },
-        //     drop: function (event, ui) {
-        //         $(this).find('p').html('dropped');
-        //         console.log('yes');
-        //         count += 1;
-        //         console.log(count);
-        //         if (count === 3) {
-        //             console.log('dead');
-        //         }
-        //     }
-        // })
-    }
 
 
 // Alert player they have won when all pieces are correctly laid
@@ -91,87 +86,3 @@ $(function () {
 // 'Reset' button to reshuffle pieces and start puzzle over again
 
 
-
-// STRETCH GOAL
-    // Player clicks on 'Start' button to begin playing
-    // Timer starts countdown when player clicks on 'Start' button 
-    // Display 'Game Over' message if timer reaches zero
-
-// STRETCH GOAL 
-// Check if piece was applied to the correct spot by applying dropped accept state
-    // Reduce incorrect guesses by 1 
-    // Check if player still has lives remaining
-    // Display 'Game Over' message if player reaches 3 incorrect guesses
-
-// STRETCH GOAL
-// Image changes when reset button pressed
-
-// ERROR HANDLING
-// Contain puzzle pieces within playing area
-
-
-
-
-
-// Declare global death counter variable
-
-// const puzzlePiece = ['a1, a2'];
-
-
-
-// Apply droppable property to piece slot
-    // $('.s1').droppable({
-    //     accept: '.a1, a2, - .a1',
-    //     drop: function (event, ui) {
-    //         $(this).find('p').html('dropped');
-    //         console.log('yes');
-    //         count += 1;
-    //         console.log(count);
-    //         if (count === 3) {
-    //             console.log('dead');
-    //         }
-    //     }    
-    // })
-
-    // $('.s2').droppable({
-    //     accept: '.a2',
-    //     drop: function (event, ui) {
-    //         $(this).find('p').html('dropped');
-    //         console.log('yes');
-    //         count += 1;
-    //         console.log(count);
-    //         if (count === 3) {
-    //             console.log('dead');
-    //         }
-    //     }
-    // })
-
-    
-    // if (count === 3) {
-    //     console.log('asdfasdf');
-    // }
-});
-
-    
-// Shuffle puzzle pieces with a randomizer
-
-
-// ERROR HANDLING
-// Contain puzzle pieces within playing area
-
-
-// Have pieces snap to the pieces slots
-
-
-// Check if piece was applied to the correct spot
-    // Reduce incorrect guesses by 1 
-    // Check if player still has lives remaining
-    // Game over if player reaches 3 incorrect guesses
-
-// Reset button restarts puzzle
-
-
-// STRETCH GOAL
-    // Add timer
-
-// `.a1, .a2, .a3, .a4, .a5, .a6, .a7, .a8, .a9, .a10, .a11, a12, .a13, .a14, .a15, .a16 - .a1`,
