23f4d88a07afe8a6463e472fdcbf7ac1acbbee1a
JonLyn <jonlyn22@gmail.com>
2020-11-11 22:31:38 -0500

    added background image positioning and fixed z-index issue :mechanic:

--- pseudoCodeWithCode.js
+++ pseudoCodeWithCode.js
@@ -1,3 +1,4 @@
+// ***** Clean code - put functions in variables and have each function only serve 1 main purpose
 
 
 // Impuzzible
@@ -5,86 +6,79 @@
 // A landing page introducing the game and rules
 // Point users to click link to go to game page
 
-// Create puzzle pieces
+// Create puzzle pieces as <div>s in HTML
 for (let i = 0; i < 16; i++) {
     $('.piecesContainer').append(`<div class='piece jigsaw${i}'></div>`);
 }
-console.log($('.piece'));
-
-// Create piece slots
+// Create piece slots as <div>s in HTML
 for (let i = 0; i < 16; i++) {
     $('.puzzleContainer').append(`<div class='pieceSlot slot${i}'></div>`);
 }
 
-// Shuffle puzzle pieces in the piece container using a randomizer
+
 // Get random x and y positioning 
 const randomX = (Math.floor(Math.random() * 150));
-
 const randomY = (Math.floor(Math.random() * 400));
 
-// Apply a unique ID/index to each puzzle piece
+
+// Insert puzzle pieces into an array to assign index numbers to each piece
 const piece = document.getElementsByClassName('piece');
 const piecesArray = Array.from(piece);
 
-console.log(piece);
 
+// Insert piece slots into an array to assign index numbers to each slot
+const slot = document.getElementsByClassName('pieceSlot')
+const slotArray = Array.from(slot);
+
+
+// Apply random start positioning to each piece
+for (let i = 0; i < 16; i++) {
+    const randomX = (Math.floor(Math.random() * 50));
+    const randomY = (Math.floor(Math.random() * 300));
+    $(piecesArray[i]).css({
+        left: randomX + 'px',
+        top: randomY + 'px'
+    })
+}  
+// TEST CODE TO CHANGE FROM ABOVE INLINE SYTLES
 // piecesArray.forEach(element => {
-
     // let baseOffset = piecesArray.offsetParent().offset();
-
     // piecesArray.offset({
     //      left: baseOffset.left + randomX,
     //      top: baseOffset.top + randomY
     //  })
 // });
 
-for (let i = 0; i < 16; i++) {
-    const randomX = (Math.floor(Math.random() * 50));
 
-    const randomY = (Math.floor(Math.random() * 300));
-
-    $(piecesArray[i]).css({
-        left: randomX + 'px',
-        top: randomY + 'px'
-    })
-}    
-
-
-// Apply a unique ID/index to each puzzle slot
-const slot = document.getElementsByClassName('pieceSlot')
-const slotArray = Array.from(slot);
-
-
-
-// Apply random positioning to each piece 
-
-
-
-// Add draggable state to puzzle pieces so they can be dragged and snap to puzzle slots
+// Assign draggable state and options to puzzle pieces so they can be dragged and snap to puzzle slots
 $('.piece').draggable({
     snap: '.pieceSlot',
-    containment: '.wrapper',
+    snapMode: 'inner',
     snapTolerance: 50,
-    zIndex: 100,
-    snapMode: 'inner'
+    containment: '.wrapper',
+    zIndex: 200
 });
 
-
-// Add droppable state to puzlze slots so they can accept correct puzzle piece
+// Create counter to keep track of the number of correctly placed pieces. Alert player that they have won once all 16 pieces have been correctly placed
 let correctCount = 0;
+if (correctCount === 16) {
+    alert('you win!');
+}
+// Create loop to assign droppable state to each puzzle slot and allow only the puzzle pice with the same array index number
 for (let i = 0; i < 16; i++) {
     $(slotArray[i]).droppable({
         accept: $(piecesArray[i]),
         drop: $(slotArray[i]).on('drop', function (event, ui) {
-            console.log('works');
-            $(piecesArray[i]).draggable('disable');
+            console.log('correct');
+            // Disable dragging the puzzle piece once it has been dropped on the correct slot and lower z-index in case another piece is placed on top
+            $(piecesArray[i]).draggable('disable').addClass('zIndexLower');
+            // Increase correct move counter by 1
+            console.log('move');
             correctCount += 1;
-            if (correctCount === 16) {
-                alert('you win!');
-            }
-            // change this to addClass
+            
+            // *****change this to addClass
             $(piecesArray[i]).css({
-                display: 'none'
+                // display: 'none'
             })
             console.log(correctCount);
         })
